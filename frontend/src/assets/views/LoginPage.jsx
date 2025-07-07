8a37344d05b2fc0ecc78034ff0dca8b094cebf3d
Boricado <fidel.mora.aguirre@gmail.com>
2025-07-07 17:51:00 -0400

    fix: API URL usando variable de entorno

--- frontend/src/assets/views/LoginPage.jsx
+++ frontend/src/assets/views/LoginPage.jsx
@@ -16,7 +16,7 @@ const LoginPage = () => {
     setError('');
 
     try {
-      const res = await axios.post('http://localhost:4000/api/login', { correo, contrasena });
+      await axios.post(`${import.meta.env.VITE_API_URL}/api/login`, { correo, contrasena });
       login(res.data);
       navigate('/');
     } catch (err) {
