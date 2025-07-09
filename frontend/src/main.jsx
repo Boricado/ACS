f10b25b4c47bf1a0d2bf068af651b505287f800b
Boricado <fidel.mora.aguirre@gmail.com>
2025-07-09 14:20:19 -0400

    bootstrap error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,8 @@ import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.jsx'; // 👈 importa el contexto
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
